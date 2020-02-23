bca5df52fb559f73d4eb59fac9682380d89db8c0
XenoAmess <xenoamess@gmail.com>
2020-02-23 16:46:14 +0800

    bugfix

--- acfun.js
+++ acfun.js
@@ -4,7 +4,7 @@
 // @version      2.1.0
 // @description  Enable Picture in Picture mode in acfun 在acfun中打开画中画模式，使chrome能够使用画中画。
 // @author       LXG_Shadow & XenoAmess
-// @match        https://www.icourse163.org/*
+// @match        https://www.acfun.cn/*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
